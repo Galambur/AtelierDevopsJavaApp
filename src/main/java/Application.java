6b1c0fa54e389cba0927c681ce7628eb7c49715f
Galambur <gaelle.derambure@epsi.fr>
2022-05-31 12:11:19 +0200

    Consulter liste des recettes et une seule recette OK

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,83 +1,85 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-
 
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        // ---- configure this for your site
-        
+        System.out.println("Bienvenue sur notre site de recette. Veuillez choisir une option en entrant au clavier");
+        System.out.println("1 - Voir la liste des recettes");
+
+        System.out.println("2 - Consulter une recette");
+        System.out.println("3 - Ajouter une recette");
+        System.out.println("4 - Supprimer une recette");
+
+        System.out.println("5 - Changer le nom d'une recette");
+        System.out.println("6 - Ajouter un ingrédient à une recette");
+        System.out.println("7 - Supprimer un ingrédient à une recette");
+
+        System.out.println("");
+
+        // todo : faire l'entrée de l'utilisateur
+        // ---- configure 
         String url = "jdbc:mysql://localhost:3306/recettes_atelier_devops?serverTimezone=UTC";
         String username = "root";
         String password = "";
 
-        String queryGetString = "SELECT * FROM ingredient;";
-        
         // ---- configure END
+        Connection con = DriverManager.getConnection(url, username, password);
+        Statement statement = con.createStatement();
 
+        if (true) {
+            showRecetteById(statement, 1);
+        }
+    }
 
-        try{
+    public static void showAllRecettes(Statement statement) throws SQLException {
+        String getAllRecettesQuery = "SELECT * FROM recette;";
+
+        ResultSet resultSet = statement.executeQuery(getAllRecettesQuery);
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("idRecette") + " - " + resultSet.getString("nom"));
+        }
+    }
+
+    public static void showRecetteById(Statement statement, int id) throws SQLException {
+        String getRecetteQuery = "SELECT * FROM recette, contient, ingredient "
+                + "WHERE recette.idRecette = contient.idRecette "
+                + "AND ingredient.idIngredient = contient.idIngredient "
+                + "AND recette.idRecette = " + id + ";";
+
+        ResultSet resultSet = statement.executeQuery(getRecetteQuery);
+        resultSet.first();
+
+        System.out.println("Recette : " + resultSet.getString("nom"));
+        System.out.println("Ingrédients nécessaires à la recette : ");
+
+        do {
+            System.out.println("\t " + resultSet.getString("nomIngredient"));
+        } while (resultSet.next());
+    }
+
+    public void exemple(String url, String username, String password, String queryGetString) {
+        try {
             Connection con = DriverManager.getConnection(url, username, password);
             Statement statement = con.createStatement();
-            
+
             ResultSet resultSet = statement.executeQuery(queryGetString);
-            
-            while (resultSet.next()){
+
+            while (resultSet.next()) {
                 System.out.println(resultSet.getInt("idIngredient") + " - " + resultSet.getString("nomIngredient"));
             }
-            
+
             String nomIngredient = "Ciboulette";
             String queryPutString = "INSERT INTO ingredient(nomIngredient) VALUES (' " + nomIngredient + "');";
             statement.execute(queryPutString);
-            
-        } catch (Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        
-        
-        
-        
-/*
-        try {
-            Class.forName("org.gjt.mm.mysql.Driver");
-        } catch (java.lang.ClassNotFoundException e) {
-            System.err.print("ClassNotFoundException: ");
-            System.err.println(e.getMessage());
-        }
-        
-        try {
-            Connection con;
-            Statement stmt;
-// Connection to the database at URL with usename and password
-            con = DriverManager.getConnection(url, username, password);
-            System.out.println("Ok, connection to the DB worked.");
-            System.out.println("Let’s see can retrieve something with: "
-                    + queryString);
-// Create a Statement Object
-            stmt = con.createStatement();
-// Send the query and bind to the result set
-            ResultSet rs = (ResultSet) stmt.executeQuery(queryString);
-            while (rs.next()) {
-                String s = rs.getString("nomIngredient");
-                float n = rs.getInt("idIngredient");
-                System.out.println(s + " " + n);
-            }
-// Close resources
-            stmt.close();
-            con.close();
-        } // print out decent error messages
-        catch (SQLException ex) {
-            System.err.println("==> SQLException: ");
-            while (ex != null) {
-                System.out.println("Message: " + ex.getMessage());
-                System.out.println("SQLState: " + ex.getSQLState());
-                System.out.println("ErrorCode: " + ex.getErrorCode());
-                ex = ex.getNextException();
-                System.out.println("");
-            }
-        }*/
     }
 }
